8b550ebec585361129df52b5a2a032f8604babaf
R-Kabilan <35830885+R-Kabilan@users.noreply.github.com>
2018-01-27 16:09:48 +0530

    Create street lights.ino

---
+++ street lights.ino
@@ -0,0 +1,21 @@
+define GND 10;
+void setup()
+{
+Serial.begin(9600);
+pinMode(5, INPUT);
+pinMode(6, INPUT);
+pinMode(7, INPUT);
+pinMode(8, INPUT);
+pinMode(9, INPUT);
+pinMode(3, OUTPUT);
+pinMode(4, OUTPUT);
+pinMode(GND, OUTPUT);
+digitalWrite(GND, LOW);
+}
+
+void loop()
+{
+//the code is pgmed to read the digital inputa and work accordingly
+
+
+}
