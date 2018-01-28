2f31ff5570bbcb1e773b45bbed596a1d57e6d0cd
R-Kabilan <35830885+R-Kabilan@users.noreply.github.com>
2018-01-28 11:59:21 +0530

    Rename air_mois.ino to DHT11_humidity.ino
    the sensor is an simple 8 bit highly accurate sensor that can be used with the help of the given library

---
+++ DHT11/DHT11_humidity.ino
@@ -0,0 +1,35 @@
+#include <dht.h>//download the DHT11 library and use the following functions
+ 
+
+ 
+#define dht_apin A0 // Analog Pin sensor is connected to
+ 
+dht DHT;
+ 
+
+void setup(){
+ 
+  Serial.begin(9600);
+ 
+ }
+ 
+
+ 
+void loop(){ 
+ 
+  
+} 
+
+float air_hum()
+{
+  DHT.read11(A4);
+ float ho=DHT.humidity;//reads humidity
+ return ho;
+}
+float air_temp()
+{
+ DHT.read11(A4);
+ float t1=DHT.temperature;//reads temperature
+ return t1; 
+}
+
